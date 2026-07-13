181ebdc1b65f79ba2dcaef25b0b262c025bfc068
Gunjit Kumar Bansal <133026796+ASH-OP@users.noreply.github.com>
2026-07-13 21:39:00 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'http://32.199.184.31:5000'
+    ],
+  }
 })
